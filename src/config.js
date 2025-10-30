7b0c37abfe5db330f751f06a9947b576b6773eee
4danielpe3 <4danielpe3@gmail.com>
2025-10-29 21:38:09 -0500

    Agg nuevas act

--- src/config.js
+++ src/config.js
@@ -7,3 +7,5 @@ export const BD_USER=process.env.BD_USER ||'root'
 export const BD_PASSWORD=process.env.BD_PASSWORD || ''
 export const BD_PORT=process.env.BD_PORT || 3306
 export const PORT=process.env.PORT || 3000
+
+export const JWT_SECRET = process.env.JWT_SECRET || '123';
