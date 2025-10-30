68bbdf304be0e437c4afe45e5d0c3beb8f0fff17
4danielpe3 <4danielpe3@gmail.com>
2025-10-29 20:46:10 -0500

    Agrego script start para Render

--- src/routes/clientesRoutes.js
+++ src/routes/clientesRoutes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 //importar las funciones
 import {/* prueba, */ getClientes, getClientesxId, postClientes, putClientesxId, deleteCliente} from '../controladores/clientesCtrl.js'
-import { verifyToken } from "../middlewares/verifyToken.js";
+import { verificarToken } from "../middlewares/verificarToken.js";
 const router=Router();
 //armar las rutas
 /* router.get('/clientes',prueba) */
-router.get('/clientes',verifyToken,getClientes)
+router.get('/clientes',verificarToken,getClientes)
 router.get('/clientes/:id',getClientesxId)
 router.post('/clientes',postClientes)
 router.put('/clientes/:id',putClientesxId)
