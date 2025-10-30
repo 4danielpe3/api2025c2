bbfc455fd356f92d5a2ae926ecb3f1863beb1891
4danielpe3 <4danielpe3@gmail.com>
2025-10-29 22:01:15 -0500

    Agg nuevas act

--- src/routes/clientesRoutes.js
+++ src/routes/clientesRoutes.js
@@ -5,7 +5,7 @@ import { verificarToken } from "../middlewares/verificarToken.js";
 const router=Router();
 //armar las rutas
 /* router.get('/clientes',prueba) */
-router.get('/clientes',getClientes)
+router.get('/clientes',verificarToken,getClientes)
 router.get('/clientes/:id',getClientesxId)
 router.post('/clientes',postClientes)   
 router.put('/clientes/:id',putClientesxId)
