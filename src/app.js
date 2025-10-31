e3b677b504746462f7fa268039c41a1d34f93fe0
4danielpe3 <4danielpe3@gmail.com>
2025-10-31 10:12:57 -0500

    Agg nuevas act

--- src/app.js
+++ src/app.js
@@ -1,24 +1,33 @@
-import express from 'express'
+import express from 'express';
 import fileUpload from 'express-fileupload';
-//importar las rutas OJO
-import clientesRoutes from './routes/clientesRoutes.js'
-import productosRoutes from './routes/productosRoutes.js'
+import cors from 'cors'; // <<-- importar cors
+
+// importar las rutas
+import clientesRoutes from './routes/clientesRoutes.js';
+import productosRoutes from './routes/productosRoutes.js';
 import loginRoutes from './routes/loginRoutes.js';
 
-const app=express();
+const app = express();
+
+// Habilitar CORS
+app.use(cors()); // permite cualquier origen
+// Si quieres restringir a tu frontend:
+// app.use(cors({ origin: 'http://localhost:8100' }));
+
 app.use(fileUpload());
 app.use(express.json());
 
-// indicar las rutas a utilizar OJO
-app.use('/api',clientesRoutes)
-app.use('/api',productosRoutes)
-app.use('/api',loginRoutes)
-app.use('/uploads', express.static('uploads'))
+// indicar las rutas a utilizar
+app.use('/api', clientesRoutes);
+app.use('/api', productosRoutes);
+app.use('/api', loginRoutes);
+app.use('/uploads', express.static('uploads'));
 
-app.use((req,resp,next)=>{
-    resp.status(400).json({
-        message: 'Endpoint not found'
-    })
-})
+// manejar endpoints no encontrados
+app.use((req, resp, next) => {
+  resp.status(400).json({
+    message: 'Endpoint not found'
+  });
+});
 
 export default app;
